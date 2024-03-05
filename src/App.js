f9230df5f08075f7aa6477fcbb2675e3579fdbe7
Manhal khwashki <manhal.akram.khwashki@gmail.com>
2024-03-06 00:18:33 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./Cart.css";
+import "./BookMarkItem.css";
+import "./Login.css";
+import Items from "./compomets/Items";
+import Appbar from "./compomets/AppBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { useDispatch } from "react-redux";
+import { LOAD_DATA } from "./Redux/actions";
+import Landing from "./compomets/Landing";
 
 function App() {
+  const dispatch = useDispatch();
+  dispatch({
+    type: LOAD_DATA,
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/labtopi"
+            element={
+              <>
+                <Appbar />
+                <Landing /> <Items />
+              </>
+            }
+          />
+          <Route
+            path="/labtopi/cart"
+            element={
+              <>
+                <Appbar /> <Cart />
+              </>
+            }
+          />
+          <Route
+            path="/labtopi/login"
+            element={
+              <>
+                <Login />
+              </>
+            }
+          />
+          <Route
+            path="/labtopi/register"
+            element={
+              <>
+                <Register />
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
